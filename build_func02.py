5b33016ec5d0246f295bc8f9214b07f63b2d52c6
allamurodxakimov <xakimov2806@gmail.com>
2023-09-22 17:41:50 -0700

    build func 2

--- build_func02.py
+++ build_func02.py
@@ -1,11 +1,4 @@
 def main():
-    """Return the value of the expression in README.md file.
-    https://github.com/codeschool43/Build_in_function_homework#build_func02 
-
-    Args:
-        None
-        
-    Returns:
-        float: the value of the expression
-    """
-    return
+    return 5
+a = main()
+print(float(a))
